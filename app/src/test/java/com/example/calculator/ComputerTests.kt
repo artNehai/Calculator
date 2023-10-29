3f7bd89a4459f9e863d48ad7337e0661a1e4a832
artNehay <artemynehay@gmail.com>
2023-10-29 19:10:12 +0300

    Update Computer's unit tests for the case when the equals button is clicked

--- app/src/test/java/com/example/calculator/ComputerTests.kt
+++ app/src/test/java/com/example/calculator/ComputerTests.kt
@@ -10,7 +10,7 @@ import org.junit.Test
 class ComputerTests {
 
     @Test
-    fun `test adding operation with positive integer`() {
+    fun `test adding operation with single integer`() {
         val computer = Computer()
 
         val fifteen = 15.0
@@ -25,34 +25,19 @@ class ComputerTests {
     }
 
     @Test
-    fun `test adding operation with negative integer`() {
-        val computer = Computer()
-
-        val minusTwo = -2.0
-        val operator = Multiply
-
-        computer.addOperation(
-            number = minusTwo,
-            operator = operator,
-        )
-
-        computer.getResult() shouldBe minusTwo
-    }
-
-    @Test
     fun `test adding chain of 3 additive operations`() {
         val computer = Computer()
 
-        val minusSix = -6.0
+        val six = 6.0
         val twelve = 12.0
         val ten = 10.0
         val operator1 = Add
         val operator2 = Subtract
         val operator3 = Multiply
-        val result = -4.0
+        val result = 8.0
 
         computer.addOperation(
-            number = minusSix,
+            number = six,
             operator = operator1,
         )
 
@@ -106,6 +91,30 @@ class ComputerTests {
     }
 
     @Test
+    fun `test adding the last operation after equals button is clicked`() {
+        val computer = Computer()
+
+        val fifteen = 15.0
+        val operator = Divide
+
+        computer.addOperation(
+            number = fifteen,
+            operator = operator,
+        )
+        computer.addOperation(
+            number = null,
+            operator = null,
+        )
+        computer.getResult() shouldBe fifteen
+
+        computer.addOperation(
+            number = fifteen,
+            operator = null,
+        )
+        computer.getResult() shouldBe fifteen
+    }
+
+    @Test
     fun `test removing last operation`() {
         val computer = Computer()
 
@@ -115,6 +124,8 @@ class ComputerTests {
         val operator2 = Add
         val result = 39.0
 
+        computer.removeLastOperation()
+        computer.getResult() shouldBe 0.0
 
         computer.addOperation(
             number = thirtyNine,
@@ -160,6 +171,8 @@ class ComputerTests {
         )
 
         computer.getResult() shouldBe result1
+        computer.removeLastOperation()
+        computer.getResult() shouldBe 0.0
 
         computer.addOperation(
             number = thirteen,
